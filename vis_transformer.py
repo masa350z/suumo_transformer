e704ec88e769b1c4ad2a297ae502f5ddbdf4e741
masa350z <msum4524@gmail.com>
2023-04-23 14:24:14 +0900

    build transformer

--- vis_transformer.py
+++ vis_transformer.py
@@ -30,9 +30,16 @@ feature_extractor = 'google/vit-base-patch16-224-in21k'
 vit_model = 'google/vit-base-patch16-224-in21k'
 feature_extractor = ViTFeatureExtractor.from_pretrained(feature_extractor)
 vit_model = ViTModel.from_pretrained(vit_model, output_attentions=True)
-
-datas = glob('datas/*')
+# %%
+datas = np.array(glob('datas/*'))
 data_y = ret_data_y(datas)
+
+dy = np.zeros(len(data_y))
+dy += data_y[:, 0] == 1
+dy[:192] += data_y[:192][:, 1] == 1
+
+data_y = data_y[dy > 0]
+datas = datas[dy > 0]
 # %%
 ar_images = []
 
@@ -65,14 +72,10 @@ class ViTNet(nn.Module):
 
         self.classifier = torch.nn.Sequential(
             torch.nn.Flatten(),
-            torch.nn.Linear(768*20, 12400),
+            torch.nn.Linear(768*20, 1024),
             torch.nn.ReLU(),
-            torch.nn.Linear(12400, 5120),
+            torch.nn.Linear(1024, 1024),
             torch.nn.ReLU(),
-            torch.nn.Linear(5120, 2560),
-            torch.nn.ReLU(),
-            torch.nn.Linear(2560, 1024),
-            torch.nn.ReLU()
         )
 
         self.output = torch.nn.Sequential(
@@ -102,20 +105,20 @@ class ViTNet(nn.Module):
 model = ViTNet()
 device = torch.device("cuda")
 model.to(device)
-# %%
+
 # まず全パラメータを勾配計算Falseにする
-"""
-for param in model.parameters():
+for param in model.vit_model.parameters():
     param.requires_grad = False
 
-# 追加したクラス分類用の全結合層を勾配計算ありに変更
-for param in model.classifier.parameters():
-    param.requires_grad = True
-"""
 optimizer = optim.Adam([
     {'params': model.parameters(), 'lr': 1e-4}
 ])
 
+"""
+optimizer = optim.Adam([
+    {'params': model.parameters(), 'lr': 1e-4}
+])
+"""
 # 損失関数
 criterion = nn.BCELoss()
 # %%
@@ -149,7 +152,7 @@ def create_dataset(data, hidden, labels):
 # %%
 dataset = create_dataset(ar_images, hidden_inp, data_y)
 
-batch_size = 1
+batch_size = 100
 
 train_size = int(0.6 * len(dataset))
 val_size = int(0.2 * len(dataset))
@@ -168,8 +171,11 @@ test_dataloader = DataLoader(test_dataset, batch_size=batch_size,
                              shuffle=True, pin_memory=True)
 # %%
 num_epochs = 10
+calc_steps = 1
+best_val_loss = float('inf')
+
 for epoch in range(num_epochs):
-    train_loss, loss = 0.0, 0.0
+    model.train()
     for i, dt in enumerate(train_dataloader):
         optimizer.zero_grad()
         inputs = dt['img']
@@ -179,19 +185,70 @@ for epoch in range(num_epochs):
         outputs = model(inputs, hidden)
 
         loss = criterion(outputs, labels)
-        if (i + 1) % 10 == 0:
+        loss.backward()  # 逆伝播を計算
+        optimizer.step()  # パラメータを更新
 
-            # 逆伝播を計算
-            loss.backward()
+        print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_dataloader)}], Train Loss: {loss.item():.4f}')
 
-            # パラメータを更新
-            optimizer.step()
+    # validation dataで評価
+    model.eval()
+    val_loss = 0.0
+    with torch.no_grad():
+        for dt in val_dataloader:
+            inputs = dt['img']
+            hidden = dt['hidden']
+            labels = dt['label']
+            inputs, hidden, labels = inputs.to(device), hidden.to(device), labels.to(device)
+            outputs = model(inputs, hidden)
+            loss = criterion(outputs, labels)
+            val_loss += loss.item()
 
-            # 学習結果を表示
-            train_loss += loss.item()
+    val_loss /= len(val_dataloader)
+    print(f'Epoch [{epoch+1}/{num_epochs}], Validation Loss: {val_loss:.4f}')
 
-            print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_dataloader)}], Train Loss: {train_loss/100:.4f}')
-            train_loss, loss = 0.0, 0.0
+    # 損失が減少していた場合はそのまま、減少していなかった場合は最良の重みをロード
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
+        torch.save(model.state_dict(), 'best_model.pt')
+    else:
+        model.load_state_dict(torch.load('best_model.pt'))
 # %%
-labels.dtype
+model.load_state_dict(torch.load('best_model.pt'))
+# %%
+model.eval()
+
+outputs_list = []
+labels_list = []
+
+for dt in test_dataloader:
+    inputs = dt['img']
+    hidden = dt['hidden']
+    labels = dt['label']
+    inputs, hidden, labels = inputs.to(device), hidden.to(device), labels.to(device)
+    outputs = model(inputs, hidden)
+
+    outputs_list.append(outputs.detach().cpu().numpy())
+    labels_list.append(labels.detach().cpu().numpy())
+
+# numpy arrayに変換
+outputs_array = np.concatenate(outputs_list, axis=0)
+labels_array = np.concatenate(labels_list, axis=0)
+
+print("Output array shape:", outputs_array.shape)
+print("Labels array shape:", labels_array.shape)
+
+# %%
+np.average(outputs_array[:,0])
+# %%
+np.average(outputs_array[:,1])
+# %%
+np.average(labels_array[:,0])
+# %%
+np.average(labels_array[:,1])
+# %%
+outputs_array*labels_array
+# %%
+outputs_array
+# %%
+
 # %%
